daad8849ace63eef287e303b22c3cd26255581a1
arunkumar <arunkumar435771@gmail.com>
2023-09-11 17:45:57 +0000

    solution

---
+++ src/components/GradientDirectionItem/index.js
@@ -0,0 +1,21 @@
+import {ListItem, ButtonEle} from './styledComponents'
+
+const GradientDirectionItem = props => {
+  const {eachItem, isActiveBtn, activeDirectionBtn} = props
+
+  const activeBtn = isActiveBtn ? 1 : 0.5
+
+  const onClickDirectionBtn = () => {
+    activeDirectionBtn(eachItem.directionId)
+  }
+
+  return (
+    <ListItem>
+      <ButtonEle bgOpacity={activeBtn} onClick={onClickDirectionBtn}>
+        {eachItem.displayText}
+      </ButtonEle>
+    </ListItem>
+  )
+}
+
+export default GradientDirectionItem
